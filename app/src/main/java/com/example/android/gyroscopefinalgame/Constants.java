58e0dd89fa0223d710245332e14d1ec66d6109e3
WhiteLotus9678 <yangw7372@my.uwstout.edu>
2018-12-07 09:50:11 -0600

    Documentation Update #1
    Updated documentation for Constants.java

--- app/src/main/java/com/example/android/gyroscopefinalgame/Constants.java
+++ app/src/main/java/com/example/android/gyroscopefinalgame/Constants.java
@@ -1,12 +1,20 @@
+/*
+Author: Will Yaj
+ */
 package com.example.android.gyroscopefinalgame;
 
 import android.content.Context;
 
 public class Constants {
+    // The width of the device's screen
     public static int SCREEN_WIDTH;
+
+    // The height of the device's screen
     public static int SCREEN_HEIGHT;
 
+    // The global infor for the app
     public static Context CURRENT_CONTEXT;
 
+    // Time the app initializes
     public static long INIT_TIME;
 }
